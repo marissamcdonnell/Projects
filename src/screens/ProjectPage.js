6aaec43f590eb29a22a122eb3fdfc8f8e33f3dd3
Marissa McDonnell <marissa.mcdonnell@fansided.com>
2019-07-28 15:29:09 -0700

    Update packagelock and finish anime

--- src/screens/ProjectPage.js
+++ src/screens/ProjectPage.js
@@ -1,9 +1,9 @@
 /* eslint-disable */
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react'
 import PortfolioLg from '../components/PortfolioLg'
 import anime from 'animejs'
 import { withAppContext } from '../components/utils/AppContext'
-import PortfolioSm from '../components/PortfolioSm';
+import PortfolioSm from '../components/PortfolioSm'
 import data from '../data/projects.json'
 
 import squish from '../images/SquishFruitscreen.png'
@@ -11,52 +11,57 @@ import nba from '../images/nba50screen.png'
 import sports from '../images/sportsFilms.png'
 
 function ProjectPage() {
-    const [img, setImage] = useState(squish)
-    const [title, setTitle] = useState(data.projects[0].title)
-    const [dataSet, setDataSet] = useState(null)
-    const images = [
-        {id: 'squish', img: squish,},
-        {id: 'nba', img: nba,},
-        {id: 'sports', img: sports,},
-    ]
+  const [img, setImage] = useState(squish)
+  const [title, setTitle] = useState(data.projects[0].title)
+  const [dataSet, setDataSet] = useState(null)
+  const [animate, setAnimate] = useState(false)
+  const images = [
+    { id: 'squish', img: squish },
+    { id: 'nba', img: nba },
+    { id: 'sports', img: sports }
+  ]
 
-    useEffect(() => {
-        if(dataSet) {
-            const image = images.filter(img => img.id === dataSet.id)
-            setImage(image[0].img)
-            setTitle(dataSet.title)
-            anime()
-        }
-        () => {}
-    }, [dataSet])
+  useEffect(() => {
+    if (dataSet) {
+      const image = images.filter(img => img.id === dataSet.id)
+      setImage(image[0].img)
+      setTitle(dataSet.title)
+      setAnimate(true)
+    }
+    ;() => {}
+  }, [dataSet])
 
-    const anime = () => {
-        console.log('anime')
-        anime({
-          targets: '.project-lg-container',
-          translateX: 500,
-        });
-      };
+  useEffect(() => {
+    if (animate) {
+      anime({
+        targets: '.project-lg-container',
+        scale: {
+          value: 1.1,
+          easing: 'easeInOutSine'
+        },
+        duration: 250,
+        direction: 'alternate',
+        loop: 1
+      })
+      setAnimate(false)
+    }
+  }, [animate])
 
-    return (
-        <div className="project footer-bottom">
-            <h2 className="title">Projects</h2>
-            <div className="projectContainer">
-                <PortfolioLg 
-                    img={img}
-                    title={title}
-                />
-                <div className="flex">
-                    {data.projects.map(data => 
-                        <button onClick={() => setDataSet(data)} type="button">
-                            <PortfolioSm data={data} />
-                        </button>
-                    )}
-                </div>
-            </div>
+  return (
+    <div className="project footer-bottom">
+      <h2 className="title">Projects</h2>
+      <div className="projectContainer">
+        <PortfolioLg img={img} title={title} />
+        <div className="flex">
+          {data.projects.map(data => (
+            <button onClick={() => setDataSet(data)} type="button">
+              <PortfolioSm data={data} />
+            </button>
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  )
 }
-
 
 export default withAppContext(ProjectPage)
