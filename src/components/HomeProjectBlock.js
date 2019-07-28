01fb594a4de8219560e5841d4133533120d6ef43
Marissa McDonnell <marissa.mcdonnell@fansided.com>
2019-07-28 15:55:16 -0700

    Add Link and button styles

--- src/components/HomeProjectBlock.js
+++ src/components/HomeProjectBlock.js
@@ -1,22 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-const HomeProjectBlock = ({featureName, projectImg, projectName}) => (
-    <div className="project-container">
-        <div className="text">
-            <h5>{featureName}</h5>
-            <h2>{projectName}</h2>
-            {/* <a className="button-lg">View Project</a> */}
-        </div>
-        <div className="box filter">
-            <img src={projectImg} alt="project 1" />
-        </div>
+const HomeProjectBlock = ({ featureName, projectImg, projectName }) => (
+  <div className="project-container">
+    <div className="text">
+      <h5>{featureName}</h5>
+      <h2>{projectName}</h2>
+      <Link className="button-lg" to="/portfolio">
+        View Project
+      </Link>
     </div>
+    <div className="box filter">
+      <img src={projectImg} alt="project 1" />
+    </div>
+  </div>
 )
 
 HomeProjectBlock.propTypes = {
-    projectImg: PropTypes.string.isRequired,
-    projectName: PropTypes.string.isRequired,
+  projectImg: PropTypes.string.isRequired,
+  projectName: PropTypes.string.isRequired
 }
 
 export default HomeProjectBlock
